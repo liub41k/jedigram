cff5c7f93eb7fae0692b2fdf6eb7f67d3f2a3f0d
Ihor Liubko <43242417+liub41k@users.noreply.github.com>
2019-07-17 14:33:56 +0300

    Updates

---
+++ precache-manifest.0a9e5f28d6378a6da569262e8450ffd1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19776ed9e377c42b93f028c66d3830de",
+    "url": "/jedigram/index.html"
+  },
+  {
+    "revision": "03bfc37184d7e18efcf2",
+    "url": "/jedigram/static/css/main.751e5b75.chunk.css"
+  },
+  {
+    "revision": "fe2e2b59a16feb3a3002",
+    "url": "/jedigram/static/js/2.4c9ecbe6.chunk.js"
+  },
+  {
+    "revision": "03bfc37184d7e18efcf2",
+    "url": "/jedigram/static/js/main.dd51b49b.chunk.js"
+  },
+  {
+    "revision": "b889633a075d94c10eb0",
+    "url": "/jedigram/static/js/runtime~main.7e4f2ff3.js"
+  },
+  {
+    "revision": "928d53e044e49e26cfc3e60260b4f6c0",
+    "url": "/jedigram/static/media/Billabong.928d53e0.woff"
+  },
+  {
+    "revision": "e730d4a242bb1c8d6b11327ec6046a97",
+    "url": "/jedigram/static/media/Billabong.e730d4a2.woff2"
+  },
+  {
+    "revision": "d75c836a1b64f6f0bad5d642f0db7c16",
+    "url": "/jedigram/static/media/ProximaNova-Bold.d75c836a.woff2"
+  },
+  {
+    "revision": "e1a6e43744cf37deeba4c32931ac02a7",
+    "url": "/jedigram/static/media/ProximaNova-Bold.e1a6e437.woff"
+  },
+  {
+    "revision": "0f93cb54280d75cd22fb3758b35ce489",
+    "url": "/jedigram/static/media/ProximaNova-BoldIt.0f93cb54.woff2"
+  },
+  {
+    "revision": "92b4fb4e78108b5942f4f47d0280e2e8",
+    "url": "/jedigram/static/media/ProximaNova-BoldIt.92b4fb4e.woff"
+  },
+  {
+    "revision": "384187825fcdf40dc3237a64465715bc",
+    "url": "/jedigram/static/media/ProximaNova-Light.38418782.woff"
+  },
+  {
+    "revision": "cf7bcffc5784522ae82a397ee7361149",
+    "url": "/jedigram/static/media/ProximaNova-Light.cf7bcffc.woff2"
+  },
+  {
+    "revision": "4ec5109c778aea9f061e9e0590526872",
+    "url": "/jedigram/static/media/ProximaNova-LightIt.4ec5109c.woff2"
+  },
+  {
+    "revision": "7dd1a0e394677c99c94a656a7e4a14e1",
+    "url": "/jedigram/static/media/ProximaNova-LightIt.7dd1a0e3.woff"
+  },
+  {
+    "revision": "2fd7777c45c62bafe05884bffcb1947d",
+    "url": "/jedigram/static/media/ProximaNova-Regular.2fd7777c.woff"
+  },
+  {
+    "revision": "695b07938615a6ed124fd69cb71cce67",
+    "url": "/jedigram/static/media/ProximaNova-Regular.695b0793.woff2"
+  },
+  {
+    "revision": "00409d6510d36743f859ea500286c37e",
+    "url": "/jedigram/static/media/ProximaNova-RegularIt.00409d65.woff2"
+  },
+  {
+    "revision": "7cda35f9353315bb484ceba3d53df95a",
+    "url": "/jedigram/static/media/ProximaNova-RegularIt.7cda35f9.woff"
+  },
+  {
+    "revision": "61cde3002f0f2a8f7f78a27a544e4fb5",
+    "url": "/jedigram/static/media/bb8.61cde300.svg"
+  },
+  {
+    "revision": "a267b4068242d2592e1886dd4d657f4f",
+    "url": "/jedigram/static/media/death-star.a267b406.svg"
+  },
+  {
+    "revision": "243f72144aa51350fc248d6bcde945a7",
+    "url": "/jedigram/static/media/yoda.243f7214.svg"
+  }
+]);
