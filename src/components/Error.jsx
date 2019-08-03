5f0c3ba0f94519c1c22c74a33b687c1541e76c6d
Ihor Liubko <43242417+liub41k@users.noreply.github.com>
2019-08-03 16:51:46 +0300

    supports for loader

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,15 +4,17 @@ import icon from '../img/error-img.png';
 
 const ErrorIndicator = () => {
 	return (
-		<div className="error-indicator">
-			<img src={icon} alt="error icon"/>
-			<span className="boom">BOOM!</span>
-			<span>
-				something has gone terribly wrong
-			</span>
-			<span>
-				(but we already sent droids to fix it)
-			</span>
+		<div className="inner centered">
+			<div className="error">
+				<img src={icon} alt="error icon"/>
+				<span className="boom">BOOM!</span>
+				<span>
+					something has gone terribly wrong
+				</span>
+				<span>
+					(but we already sent droids to fix it)
+				</span>
+			</div>
 		</div>
 	);
 };
